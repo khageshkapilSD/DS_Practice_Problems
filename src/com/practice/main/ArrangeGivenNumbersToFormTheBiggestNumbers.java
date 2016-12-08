636a995ab09a6e869ea0d42cc2f484b006a7188c
Khagesh Kapil <kkapil@adobe.com>
2016-12-08 16:10:12 +0530

    Correcting the comparison logic in MyComarator, earlier it was lexicographical comparison in String, now it has been made to Integer to integer comarison. Also, added comments about explanation on approach.

--- src/com/practice/main/ArrangeGivenNumbersToFormTheBiggestNumbers.java
+++ src/com/practice/main/ArrangeGivenNumbersToFormTheBiggestNumbers.java
@@ -3,22 +3,31 @@ package com.practice.main;
 import java.util.Arrays;
 import java.util.Comparator;
  
-class MyComparator implements Comparator<Integer>{
+/*
+ * A simple solution that comes to our mind is to sort all numbers in descending order, 
+ * but simply sorting doesn’t work. For example, 548 is greater than 60, but in output 60 comes before 548. 
+ * As a second example, 98 is greater than 9, but 9 comes before 98 in output.
+ * So how do we go about it? The idea is to use any comparison based sorting algorithm. 
+ * In the used sorting algorithm, instead of using the default comparison, 
+ * write a comparison function myCompare() and use it to sort numbers. 
+ * Given two numbers X and Y, how should myCompare() decide which number to put first – 
+ * we compare two numbers XY (Y appended at the end of X) and YX (X appended at the end of Y). 
+ * If XY is larger, then X should come before Y in output, else Y should come before. 
+ * For example, let X and Y be 542 and 60. To compare X and Y, we compare 54260 and 60542.
+ * Since 60542 is greater than 54260, we put Y first.
+ */
+
+class MyComparator implements Comparator<Integer> {
 	public int compare(Integer a, Integer b) {
- 
-		String str1 = a.toString() + b.toString();
-		String str2 = b.toString() + a.toString();
- 
-		if(str1.compareTo(str2) > 0)
-			return -1;
-		else
-			return 1;
+		Integer choice1 = Integer.valueOf(a.toString() + b.toString());
+		Integer choice2 = Integer.valueOf(b.toString() + a.toString());
+		return choice1.compareTo(choice2);
 	}
 }
  
 public class ArrangeGivenNumbersToFormTheBiggestNumbers {
  
-	static void arrangeGivenNumbersToFormTheBiggestNumbers(int[] arr) {
+	static int[] arrangeGivenNumbersToFormTheBiggestNumbers(int[] arr) {
 		int n = arr.length;
  
 		Integer[] arr1 = new Integer[n];
@@ -29,11 +38,9 @@ public class ArrangeGivenNumbersToFormTheBiggestNumbers {
 		Arrays.sort(arr1,new MyComparator());
  
 		for(int i=0;i<n;i++)
-			arr[i] = arr1[i];
+			arr[i] = arr1[n-1-i];
  
-		for(int i=0;i<n;i++)
-			System.out.print(arr[i]);
-		System.out.println();
+		return arr;
 	}
  
 	static void printArray(int[] arr) {
@@ -43,10 +50,9 @@ public class ArrangeGivenNumbersToFormTheBiggestNumbers {
 	}
  
 	public static void main(String[] args) {
-		int[] arr = {54, 546, 548, 60};
- 
+		int[] arr = {548, 60, 54, 546};
 		printArray(arr);
- 
 		arrangeGivenNumbersToFormTheBiggestNumbers(arr);
+		printArray(arr);
 	}
 }
